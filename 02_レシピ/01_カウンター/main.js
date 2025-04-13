9068a2dbe10fc1e92904d89ca535d21fdafb233b
nonou-u0 <tokyogulu0906@gmail.com>
2025-04-13 18:21:44 +0900

    [3の倍数]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,12 +1,19 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const doubleButton = document.getElementById("double-button")
 
-let count = 0
+let count = 1
 
 // ボタン要素のonclickハンドラに関数を代入する
 plusButton.onclick = function () {
   // count を更新
-  count += 1
+  count -= 1
+  // count を表示
+  display.textContent = count
+}
+doubleButton.onclick = function () {
+  // count を更新
+  count *= 2
   // count を表示
   display.textContent = count
 }
